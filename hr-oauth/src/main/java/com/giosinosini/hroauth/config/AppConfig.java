103096ff7c63ed47d24f24fc208beebbf7e5413e
gitName <carvalho.sino@gmail.com>
2021-10-26 23:26:31 +0100

    Login, JWT token

---
+++ hr-oauth/src/main/java/com/giosinosini/hroauth/config/AppConfig.java
@@ -0,0 +1,30 @@
+package com.giosinosini.hroauth.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+@Configuration
+public class AppConfig {
+	
+	@Bean
+	public BCryptPasswordEncoder bCryptPasswordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	@Bean
+	public JwtAccessTokenConverter AccessTokenConverter() {
+		JwtAccessTokenConverter tokenConverter = new JwtAccessTokenConverter();
+		tokenConverter.setSigningKey("MY-SECRET-KEY"); //temporary test
+		return tokenConverter;
+	}
+	
+	@Bean
+	public JwtTokenStore tokenStore() {  // read information from token
+		return new JwtTokenStore(AccessTokenConverter());
+	}
+	
+
+}
