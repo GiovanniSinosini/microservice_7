c17a63e5764377fd0fa10a3b0abc191621112574
gitName <carvalho.sino@gmail.com>
2021-10-27 23:02:36 +0100

    Security configs to config server

--- hr-api-gateway-zuul/src/main/java/com/giosinosini/hrapigatewayzuul/config/AppConfig.java
+++ hr-api-gateway-zuul/src/main/java/com/giosinosini/hrapigatewayzuul/config/AppConfig.java
@@ -1,17 +1,23 @@
 package com.giosinosini.hrapigatewayzuul.config;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
+@RefreshScope
 @Configuration
 public class AppConfig {
+	
+	@Value("${jwt.secret}")
+	private String jwtSecret;
 	
 	@Bean
 	public JwtAccessTokenConverter AccessTokenConverter() {
 		JwtAccessTokenConverter tokenConverter = new JwtAccessTokenConverter();
-		tokenConverter.setSigningKey("MY-SECRET-KEY"); //temporary test
+		tokenConverter.setSigningKey(jwtSecret);
 		return tokenConverter;
 	}
 	
